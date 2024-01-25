6e24bd0e635021cc2dfe4d18873aed5302fcc533
balasai8900 <118534815+balasai8900@users.noreply.github.com>
2024-01-25 12:36:26 +0530

    Update SampleProjectApplication.java

--- src/main/java/com/pack/fabo/SampleProjectApplication.java
+++ src/main/java/com/pack/fabo/SampleProjectApplication.java
@@ -4,7 +4,6 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-@ContextConfiguration
 public class SampleProjectApplication {
 
 	public static void main(String[] args) {
